3bfd50bb56e64091b18b710b301b84c2540d616e
AntarikshPangea <antariksh.alse@pangeatech.net>
2024-06-25 11:57:26 +0530

    Add: Lint Setup

---
+++ lib/pages/AuthPage.tsx
@@ -0,0 +1,9 @@
+// Auth page
+
+export default function AuthPage() {
+  return (
+    <div>
+      <h1>Auth Page</h1>
+    </div>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from 'pangea_ui_library'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button } from 'pangea_ui_library';
+import { StyledEngineProvider } from '@mui/material';
+import { AuthPage } from 'pangea_ui_library/page';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className=''>
-        <Button className='text-red-400'>Button</Button>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <StyledEngineProvider injectFirst>
+      <div className="">
+        <Button className="bg-purple-300">Button</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h1>Ui Library</h1>
+      <AuthPage />
+    </StyledEngineProvider>
+  );
 }
 
-export default App
+export default App;
