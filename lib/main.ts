a68324a6987f0df430b16289fe557cd850e2331a
Suraj <suraj.rao@pangeatech.net>
2024-06-25 13:22:41 +0530

    textfield component

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,3 @@
 export {default as Button} from "./components/Button"
 export {default as Dropdown} from './components/Dropdown'
+export {default as TextField} from './components/TextField'
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { Button } from 'pangea_ui_library';
-import { StyledEngineProvider } from '@mui/material';
-import { AuthPage } from 'pangea_ui_library/page';
+import { useState } from 'react'
+import { Button, Dropdown } from 'pangea_ui_library'
+import TextField from 'pangea_ui_library/components/TextField';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const options = ['Option 1', 'Option 2', 'Option 3'];
+  const [value, setValue] = useState('');
+  const [error, setError] = useState('');
+  const [flag, setFlag] = useState(true); 
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    if (event.target.value === '') {
+      setError('This field is required');
+    } else {
+      setError('');
+    }
+  };
+
+  const toggleFlag = () => {
+    setFlag((prevFlag) => !prevFlag);
+  };
 
   return (
-    <StyledEngineProvider injectFirst>
-      <div className="">
-        <Button className="bg-purple-300">Button</Button>
+    <>
+      <div className=''>
+      <TextField
+        label="Example Label"
+        value={value}
+        onChange={handleChange}
+        errormsg={error}
+        flag={flag} // Set the flag dynamically
+      />
+      <button onClick={toggleFlag}>
+        {flag ? 'Disable' : 'Enable'} TextField
+      </button>
+        <Button className='bg-red-700'>Button</Button>
+        <Dropdown label="Example Dropdown" options={options} value={value} onChange={(e) => setValue(e.target.value)} />
       </div>
-      <h1>Ui Library</h1>
-      <AuthPage />
-    </StyledEngineProvider>
-  );
+    </>
+  )
 }
 
 export default App;
